8660dedb5eab91d5a873beeac39131dfc34e01e6
Anh <vongmaianh170196@gmail.com>
2019-10-23 16:01:04 +0300

    react and redux set up

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Alert from './components/layouts/Alert';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import './App.css';
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+  <Router>
+    <Fragment>
+      <Navbar/>
+      <Route exact path="/" component={Landing}/>      
+      <section className="container">
+        <Alert/>
+        <Switch>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+  </Provider>
+);
+
+export default App;
